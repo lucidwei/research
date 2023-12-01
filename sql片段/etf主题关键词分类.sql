3b661437b5769ce79a7e42262139d596675179f2
lucidwei <289366948@qq.com>
2023-12-01 19:40:20 +0800

    refine etf分类 sql

--- sql片段/etf主题关键词分类.sql
+++ sql片段/etf主题关键词分类.sql
@@ -8,8 +8,8 @@ WITH IndexClassification AS (
             WHEN fund_fullname SIMILAR TO '%(消费)%' THEN '消费'   
 			WHEN fund_fullname SIMILAR TO '%(上海金|黄金)%' THEN '黄金'   
 	WHEN fund_fullname SIMILAR TO '%(大盘)%' THEN '大盘'   
-	WHEN fund_fullname SIMILAR TO '%(中盘)%' THEN '中盘'   
-	WHEN fund_fullname SIMILAR TO '%(小盘|国证2000)%' THEN '小盘'   
+	WHEN fund_fullname SIMILAR TO '%(中盘|中证500|国证1000)%' THEN '中盘'
+	WHEN fund_fullname SIMILAR TO '%(小盘|国证2000|中证1000|中证2000|科创100)%' THEN '小盘'
 	WHEN fund_fullname SIMILAR TO '%(创业板)%' THEN '创业板'   
 	WHEN fund_fullname SIMILAR TO '%(科创板)%' THEN '科创板'
 	WHEN fund_fullname SIMILAR TO '%(科创创业)%' THEN '科创创业'
@@ -17,7 +17,7 @@ WITH IndexClassification AS (
         END AS predicted_etf_type
     FROM public.product_static_info
     WHERE product_type = 'fund'
-      AND fund_fullname NOT LIKE '%债%'
+      AND fund_fullname NOT SIMILAR TO '%(货币|债)%'
       AND fund_fullname NOT LIKE '%QDII%'
       AND fund_fullname LIKE '%交易型开放式%'
       AND etf_type IS NULL
