dfef0679ce191e3940d2dbddf325a23221f525ea
lucidwei <289366948@qq.com>
2023-12-12 13:48:30 +0800

    add daily update bat for任务计划程序

---
+++ bat_scripts/run_daily_update.bat
@@ -0,0 +1,3 @@
+@echo off
+D:\ProgramData\anaconda3\envs\touyan\python.exe E:\BaiduNetdiskWorkspace\FICC_research\daily_update.py
+pause
--- daily_update.py
+++ daily_update.py
@@ -3,10 +3,16 @@
 # Author  : Lucid
 # FileName: daily_update.py
 # Software: PyCharm
+import datetime
 from base_config import BaseConfig
 from prj_equity_liquidity.db_updater import DatabaseUpdater as LiquidityDatabaseUpdater
 from prj_equity_sentiment.db_updater import DatabaseUpdater as SentimentDatabaseUpdater
 from prj_equity_sentiment.processor import Processor
+
+# 获取当前时间
+current_time = datetime.datetime.now()
+# 设置您希望比较的时间（上午9点）
+target_time = datetime.datetime.now().replace(hour=9, minute=0, second=0, microsecond=0)
 
 base_config_liquidity = BaseConfig('equity_liquidity')
 data_updater = LiquidityDatabaseUpdater(base_config_liquidity)
@@ -15,7 +21,10 @@ data_updater.all_funds_info_updater.update_all_funds_info()
 data_updater.etf_lof_updater.logic_etf_lof_funds()
 # 北向晚上8点还没出，估计也得第二天出
 data_updater.north_inflow_updater.logic_north_inflow_by_industry()
-data_updater.margin_trade_by_industry_updater.logic_margin_trade_by_industry()
+# 比较当前时间是否晚于或等于上午9点
+if current_time >= target_time:
+    # 如果是，执行函数
+    data_updater.margin_trade_by_industry_updater.logic_margin_trade_by_industry()
 
 base_config_sentiment = BaseConfig('equity_sentiment')
 SentimentDatabaseUpdater(base_config_sentiment)
