53cdd7ec01dd2e5c9edeca8fe4b4b95b73ff5ccc
lucidwei <289366948@qq.com>
2024-03-08 14:39:08 +0800

    edit irf backtest

--- prj_quarterly/irf_backtest.py
+++ prj_quarterly/irf_backtest.py
@@ -11,12 +11,14 @@ import matplotlib, os
 import matplotlib.gridspec as gridspec
 from base_config import BaseConfig
 from pgdb_updater_base import PgDbUpdaterBase
+from 基金总仓位测算demo import CalcFundPosition
 from sqlalchemy import text
 
 
 class DataProcessor(PgDbUpdaterBase):
     def __init__(self, base_config: BaseConfig):
         super().__init__(base_config)
+        self.base_config = base_config
         self.load_from_pgdb()
         self.load_from_excel()
         self.process_into_df()
@@ -26,14 +28,52 @@ class DataProcessor(PgDbUpdaterBase):
         从21下半年开始的
         pg数据包括：
         北向、两融、ETF日度净流入
+        行业收盘价
         """
-        pass
+        north_inflow_long = self.select_df_from_long_table(table_name='markets_daily_long',
+                                                           field='净买入')
+        margin_long = self.select_df_from_long_table(table_name='markets_daily_long',
+                                                           field='融资净买入额')
+        etf_long = self.read_joined_table_as_dataframe(target_table_name='markets_daily_long',
+                                                       target_join_column='product_static_info_id',
+                                                       join_table_name='product_static_info',
+                                                       join_column='internal_id',
+                                                       filter_condition="field='净流入额' AND etf_type='行业ETF'")
+        north_inflow_long['行业'] = north_inflow_long['product_name'].str.extract(r'CS(\w+)', expand=False)
+        df_north_inflow = north_inflow_long.pivot(index='date', columns='行业', values='value') / 1e8
+        self.df_north_inflow = df_north_inflow.fillna(0)
+
+        margin_long['行业'] = margin_long['product_name'].str.extract(r'CS(\w+)', expand=False)
+        df_margin = margin_long.pivot(index='date', columns='行业', values='value') / 1e8
+        self.df_margin = df_margin.fillna(0)
+
+        grouped_etf = etf_long.groupby(['date', 'stk_industry_cs'])['value'].sum().reset_index()
+        df_etf = grouped_etf.pivot(index='date', columns='stk_industry_cs', values='value') / 1e8
+        self.df_etf = df_etf.fillna(0)
+
+        df_price_joined = self.read_joined_table_as_dataframe(
+            target_table_name='markets_daily_long',
+            target_join_column='product_static_info_id',
+            join_table_name='product_static_info',
+            join_column='internal_id',
+            filter_condition="field='收盘价'"
+        )
+        df_price_joined = df_price_joined[['date', 'chinese_name', 'field', 'value']].rename(columns={'chinese_name': 'industry'})
+        df_daily_price = df_price_joined.pivot(index='date', columns='industry', values='value')
+        self.df_daily_price = df_daily_price
+        self.df_daily_return = df_daily_price.pct_change().dropna(how='all')*100
+
 
     def load_from_excel(self):
         """
         调用基金总仓位测算demo.py中的方法得到每日基金的估算行业净流入
         """
-        pass
+        obj = CalcFundPosition(self.base_config, '18q4')
+
+        state_estimates_post, return_errors = obj.post_constraint_kf(obj.industry_return, obj.total_return,
+                                                                     calibrate=True)
+        active_adjustments = obj.calculate_active_adjustment(state_estimates_post, obj.industry_return)
+        self.df_fund_estimate = obj.calculate_active_adjustment_amount(active_adjustments)
 
     def process_into_df(self):
         pass
@@ -60,3 +100,7 @@ class Evaluator:
 
     def calc_backtest_nav(self):
         pass
+
+
+base_config = BaseConfig('quarterly')
+data = DataProcessor(base_config)
