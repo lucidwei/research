9623bb9e608bd8231835f1fff8a64522945ea1f2
lucidwei <289366948@qq.com>
2024-03-08 19:37:04 +0800

    精简多余代码

--- prj_quarterly/irf_backtest.py
+++ prj_quarterly/irf_backtest.py
@@ -6,11 +6,6 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from datetime import timedelta
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import matplotlib, os
-import matplotlib.gridspec as gridspec
-from typing import Optional
 from base_config import BaseConfig
 from pgdb_updater_base import PgDbUpdaterBase
 from 基金总仓位测算demo import CalcFundPosition
@@ -24,12 +19,6 @@ class DataProcessor(PgDbUpdaterBase):
         self.load_from_pgdb()
         self.load_from_excel()
         self.align_dates_industries()
-        # self.df_daily_price: Optional[pd.DataFrame] = None
-        # self.df_daily_return: Optional[pd.DataFrame] = None
-        # self.df_etf: Optional[pd.DataFrame] = None
-        # self.df_north_inflow: Optional[pd.DataFrame] = None
-        # self.df_margin: Optional[pd.DataFrame] = None
-        # self.df_fund_estimate: Optional[pd.DataFrame] = None
 
     def load_from_pgdb(self):
         """
@@ -112,10 +101,10 @@ class DataProcessor(PgDbUpdaterBase):
 class Evaluator:
     def __init__(self, data: DataProcessor):
         self.data = data
-        self.analyze_industries()
         self.calc_weekly_irfs()
         self.evaluate_weekly_spearman()
         self.calc_backtest_nav()
+
 
     def calc_weekly_irfs(self):
         """
@@ -123,87 +112,6 @@ class Evaluator:
         用不同资金流估算出各个行业涨跌幅，ETF和基金估算的权重低一些（前者数据点少，后者）得到行业涨跌幅排序
         与下一周实际行业涨跌幅顺序做秩相关。
         """
-
-        # end_date = pd.to_datetime('today')
-        # start_date = end_date - pd.Timedelta(days=60)
-
-        # 从每个数据帧中选择过去60天的数据
-        df_daily_return = self.data.df_daily_return
-        df_daily_price = self.data.df_daily_price
-        df_north_inflow = self.data.df_north_inflow
-        df_margin = self.data.df_margin
-        df_etf = self.data.df_etf
-        df_fund_estimate = self.data.df_fund_estimate
-
-        # 计算每周收益率
-        # next_week_returns = df_daily_price.pct_change(5).shift(-5)  # 预测下一“周”的收益率
-
-        # 日期索引转换为周期
-        period_starts = pd.date_range(start=df_daily_price.index.min(),
-                                      end=df_daily_price.index.max() - timedelta(days=60), freq='5D')
-
-        correlations = []
-
-        for history_start_date in period_starts:
-            history_end_date = history_start_date + timedelta(days=60)  # 使用60天的数据
-            # next_week_date = history_end_date + timedelta(days=5)  # 下一“周”日期
-            next_week_start_date = history_end_date + timedelta(days=1)
-            next_week_end_date = next_week_start_date + timedelta(days=4)
-
-            # 计算到最后一周 循环终止
-            if next_week_end_date > df_daily_price.index.max():
-                break
-
-            # 获取下一周期（“周”）的实际涨跌幅
-            history_return = df_daily_return.loc[history_start_date:history_end_date]
-            next_week_return = df_daily_return.loc[next_week_start_date:next_week_end_date].sum()
-
-            irf_results = {}
-
-            # 对每个行业进行分析
-            for industry in df_daily_return.columns:
-                industry_data = {
-                    'north_inflow': df_north_inflow[industry].loc[history_start_date:history_end_date],
-                    'margin': df_margin[industry].loc[history_start_date:history_end_date],
-                    'etf': df_etf[industry].loc[history_start_date:history_end_date],
-                    'fund_estimate': df_fund_estimate[industry].loc[history_start_date:history_end_date]
-                }
-                industry_inflow_history = pd.DataFrame(industry_data)
-                merged = pd.merge(history_return[industry], industry_inflow_history,
-                              left_index=True, right_index=True)
-
-                if industry_inflow_history.dropna().shape[0] < 25:  # 确保有足够的数据点进行VAR分析
-                    continue
-
-                # try:
-                # model = sm.tsa.VAR(merged)
-                # results = model.fit(maxlags=5, ic='aic')
-                # print(f'选定的最优滞后期: {results.k_ar}')
-                # irf = results.irf(10).irfs#[:, :, -1]  # 取对下周收益率的IRF
-                # irf_sum = np.sum(irf, axis=0)
-                # irf_results[industry] = np.sum(irf_sum)  # 求和得到整体影响
-                # except Exception as e:
-                #     print(f"Error in VAR model for {industry}: {e}")
-                #     continue
-
-            # 根据IRF的结果排序行业
-            sorted_industries = sorted(irf_results, key=irf_results.get, reverse=True)
-
-            # 获取下一周期实际涨跌幅的排序
-            sorted_actual_returns = next_week_return.sort_values(ascending=False).index.tolist()
-
-            # 计算实际涨跌幅排序
-            sorted_real_returns = next_week_return.sort_values(ascending=False).index.tolist()
-
-            # 计算秩相关
-            if sorted_industries and sorted_real_returns:
-                rank_corr, _ = spearmanr(sorted_industries, sorted_real_returns)
-                correlations.append(rank_corr)
-
-        # 返回所有周期的秩相关平均值
-        return np.mean(correlations)
-
-    def analyze_industries(self):
         df_daily_return = self.data.df_daily_return
         df_daily_price = self.data.df_daily_price
         df_north_inflow = self.data.df_north_inflow
@@ -288,9 +196,9 @@ class Evaluator:
         try:
             model = sm.tsa.VAR(merged)
             results = model.fit(maxlags=5, ic='aic')
-            irf = results.irf(10).irfs
+            irf = results.irf(5).irfs
             # 获取冲击变量（资金流入）第1列对响应变量（行业收益率）第0列的累积影响
-            cumulative_response = np.sum(irf[:10, 1, 0])
+            cumulative_response = np.sum(irf[:5, 1, 0])
             return cumulative_response
         except Exception as e:
             print(f"Error in VAR model for {fund_flow_name} {industry}: {e}")
