3371cd131177b15a5ea90a6ac061b11e5c896a88
lucidwei <289366948@qq.com>
2024-04-10 16:35:23 +0800

    enhance data visualizer

--- prj_boom/data_visualizer.py
+++ prj_boom/data_visualizer.py
@@ -7,7 +7,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import set_index_col_wind
-
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['STZhongsong']    # 指定默认字体：解决plot不能显示中文问题
+mpl.rcParams['axes.unicode_minus'] = False           # 解决保存图像是负号'-'显示为方块的问题
 
 class DataVisualizer:
     def __init__(self, file_path, sheet_name):
@@ -21,7 +23,7 @@ class DataVisualizer:
         self.file_path = file_path
         self.sheet_name = sheet_name
         self.df = pd.read_excel(file_path, sheet_name=sheet_name)
-        self.df_list = self._split_dataframe()
+        self.df_dict = self._split_dataframe()
 
     def _split_dataframe(self):
         """
@@ -48,48 +50,47 @@ class DataVisualizer:
                     if df_name == '基本面':
                         sub_df = sub_df.replace(0, np.nan)
 
-                    df_dict[df_name] = sub_df
+                    df_dict[df_name] = sub_df.astype(float)
                 start_idx = idx + 1
 
         return df_dict
 
-    def plot_data(self, df1_idx, df2_idx, df1_col, df2_col, start_date=None, end_date=None):
+    def plot_data(self, df1_key, df2_key, df1_col, df2_col, start_date=None, end_date=None):
         """
         绘制两个 DataFrame 中指定列的数据。
 
         参数:
-        - df1_idx: 第一个 DataFrame 在 df_list 中的索引。
-        - df2_idx: 第二个 DataFrame 在 df_list 中的索引。
+        - df1_key: 第一个 DataFrame 的键。
+        - df2_key: 第二个 DataFrame 的键。
         - df1_col: 第一个 DataFrame 中要绘制的列名。
         - df2_col: 第二个 DataFrame 中要绘制的列名。
         - start_date: 可选的起始日期,用于过滤数据。
         - end_date: 可选的结束日期,用于过滤数据。
         """
-        df1 = self.df_list[df1_idx]
-        df2 = self.df_list[df2_idx]
+        df1 = self.df_dict[df1_key]
+        df2 = self.df_dict[df2_key]
 
-        if 'Date' in df1.columns:
-            df1.set_index('Date', inplace=True)
-        elif '时间区间' in df1.columns:
-            df1.set_index('时间区间', inplace=True)
+        # 检查指定的列名是否存在于对应的 DataFrame 中
+        if df1_col not in df1.columns:
+            raise ValueError(f"Column '{df1_col}' does not exist in DataFrame '{df1_key}'.")
+        if df2_col not in df2.columns:
+            raise ValueError(f"Column '{df2_col}' does not exist in DataFrame '{df2_key}'.")
 
-        if 'Date' in df2.columns:
-            df2.set_index('Date', inplace=True)
-        elif '时间区间' in df2.columns:
-            df2.set_index('时间区间', inplace=True)
+        # 将两个 DataFrame 按日期索引合并,并填充缺失值
+        merged_df = pd.merge(df1[[df1_col]], df2[[df2_col]], left_index=True, right_index=True, how='outer').dropna()
+        # merged_df.fillna(method='ffill', inplace=True)
+        # merged_df.fillna(method='bfill', inplace=True)
 
         if start_date:
-            df1 = df1.loc[start_date:]
-            df2 = df2.loc[start_date:]
+            merged_df = merged_df.loc[start_date:]
         if end_date:
-            df1 = df1.loc[:end_date]
-            df2 = df2.loc[:end_date]
+            merged_df = merged_df.loc[:end_date]
 
         fig, ax1 = plt.subplots(figsize=(10, 6))
         ax2 = ax1.twinx()
 
-        ax1.plot(df1[df1_col], label=df1_col)
-        ax2.plot(df2[df2_col], label=df2_col, color='red')
+        ax1.plot(merged_df[df1_col], label=df1_col)
+        ax2.plot(merged_df[df2_col], label=df2_col, color='red')
 
         ax1.set_xlabel('Date')
         ax1.set_ylabel(df1_col)
@@ -103,4 +104,4 @@ class DataVisualizer:
 
 # 使用示例
 visualizer = DataVisualizer(rf"D:\WPS云盘\WPS云盘\工作-麦高\专题研究\景气研究\行业景气数据库与展示.xlsx", '石油石化')
-visualizer.plot_data(0, 2, '营业收入(同比增长率)', '中国:规模以上工业增加值:石油和天然气开采业:当月同比', start_date='2022-01-01', end_date='2023-12-31')
+visualizer.plot_data('财务', '基本面', '营业收入(同比增长率)', '中国:规模以上工业增加值:石油和天然气开采业:当月同比')#, start_date='2022-01-01', end_date='2023-12-31')
